4f6d11f87d919e76fa269f53b34960bdc32c2b5e
Godson Pius <40927597+godson-pius@users.noreply.github.com>
2024-03-14 12:04:28 +0100

    fixed page

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -4,12 +4,17 @@ import Sidebar from '@/components/admin/Sidebar'
 import React from 'react'
 import {
     Card,
-    CardHeader,
     CardBody,
     CardFooter,
     Typography,
+    ListItemSuffix,
     Button,
-} from "@material-tailwind/react";
+    Accordion,
+    AccordionHeader,
+    AccordionBody
+  } from "@material-tailwind/react";
+  import {IoAddOutline, IoAppsOutline, IoArrowDown, IoBandage, IoBriefcase, IoCalendarOutline, IoCart, IoCashOutline, IoDocumentTextOutline, IoHome, IoMedkit, IoMedkitOutline, IoPeople, IoPersonAdd, IoPersonAddOutline, IoRefreshOutline, IoRepeatOutline, IoServerOutline, IoThermometer } from 'react-icons/io5';
+
 
 const page = () => {
     return (
@@ -19,98 +24,153 @@ const page = () => {
                 <div className='w-full'>
                     <AdminNavbar />
 
-                    <div className='lg:grid lg:grid-cols-4 gap-4 px-5'>
+                    <div className='grid grid-cols-5 gap-3'>
                         {/* Content goes here */}
-                        <Card className="w-full mt-10">
-                            <CardHeader
-                                variant="gradient"
-                                color="red"
-                                className="mb-4 grid h-16 place-items-center"
-                            >
-                                <Typography variant="h3" color="white">
-                                    Antenatal Patients
+                        <Card className="mt-6 ml-6 h-36 w-56 flex-col justify-center items-center bg-red-500 rounded-md">
+                            <CardBody>
+                                <Typography variant="h1" color="blue-gray" className="7xl font-medium text-4xl text-white justify-center text-center">
+                                3,000
                                 </Typography>
-                            </CardHeader>
-                            <CardBody className="flex flex-col gap-4">
-                                <Typography variant='h1' className='text-center'>3098</Typography>
                             </CardBody>
-                            <CardFooter className="pt-0">
-                                <Typography variant='small'>Antenatal Patients</Typography>
-                            </CardFooter>
-                        </Card>
-
-                        <Card className="w-full mt-10">
-                            <CardHeader
-                                variant="gradient"
-                                color="purple"
-                                className="mb-4 grid h-16 place-items-center"
-                            >
-                                <Typography variant="h3" color="white">
-                                    GOPD
+                            <div className='bg-red-600 w-full py-4'>
+                                <Typography className='justify-center text-center text-lg text-white'>
+                                Antenatal Patients
                                 </Typography>
-                            </CardHeader>
-                            <CardBody className="flex flex-col gap-4">
-                                <Typography variant='h1' className='text-center'>3847</Typography>
-                            </CardBody>
-                            <CardFooter className="pt-0">
-                                <Typography variant='small'>GOPD</Typography>
-                            </CardFooter>
-                        </Card>
+                            </div>
+                            </Card>
 
-                        <Card className="w-full mt-10">
-                            <CardHeader
-                                variant="gradient"
-                                color="green"
-                                className="mb-4 grid h-16 place-items-center"
-                            >
-                                <Typography variant="h3" color="white">
+                            <Card className="mt-6 h-36 w-56 flex-col justify-center items-center bg-purple-400  rounded-md">
+                            <CardBody>
+                                <Typography variant="h1" color="blue-gray" className="font-medium text-4xl text-white ">
+                                3,847
+                                </Typography>
+                            </CardBody>
+                            <div className='bg-purple-600 w-full py-4'>
+                                <Typography className='justify-center text-center text-lg text-white'>
+                                GOPD
+                                </Typography>
+                            </div>
+                            </Card>
+
+                            <Card className="mt-6 h-36 w-56 flex-col justify-center items-center bg-green-400 rounded-md">
+                            <CardBody>
+                                <Typography variant="h1" color="blue-gray" className="font-medium text-4xl text-white">
+                                2
+                                </Typography>
+                            </CardBody>
+                            <div className='bg-green-600 w-full py-4'>
+                                <Typography className='justify-center text-center text-lg text-white'>
                                 Laparoscopic
                                 </Typography>
-                            </CardHeader>
-                            <CardBody className="flex flex-col gap-4">
-                                <Typography variant='h1' className='text-center'>2</Typography>
-                            </CardBody>
-                            <CardFooter className="pt-0">
-                                <Typography variant='small'>Laparoscopic</Typography>
-                            </CardFooter>
-                        </Card>
+                            </div>
+                            </Card>
 
-                        <Card className="w-full mt-10">
-                            <CardHeader
-                                variant="gradient"
-                                color="orange"
-                                className="mb-4 grid h-16 place-items-center"
-                            >
-                                <Typography variant="h3" color="white">
-                                Orthopedic
+                            <Card className="mt-6 h-36 w-56 flex-col justify-center items-center bg-orange-400 rounded-md">
+                            <CardBody>
+                                <Typography variant="h1" color="blue-gray" className="font-medium text-4xl text-white">
+                                0
                                 </Typography>
-                            </CardHeader>
-                            <CardBody className="flex flex-col gap-4">
-                                <Typography variant='h1' className='text-center'>0</Typography>
                             </CardBody>
-                            <CardFooter className="pt-0">
-                                <Typography variant='small'>Orthopedic</Typography>
-                            </CardFooter>
-                        </Card>
-                        
-                        <Card className="w-full mt-10 col-span-4">
-                            <CardHeader
-                                variant="gradient"
-                                color="gray"
-                                className="mb-4 grid h-16 place-items-center"
-                            >
-                                <Typography variant="h3" color="white">
+                            <div className='bg-orange-600 w-full py-4'>
+                                <Typography className='justify-center text-center text-lg text-white'>
+                                Orthopaedic
+                                </Typography>
+                            </div>
+                            </Card>
+
+                            <Card className="mt-6 h-36 w-56 flex-col justify-center items-center bg-gray-400 rounded-md">
+                            <CardBody>
+                                <Typography variant="h1" color="blue-gray" className="font-medium text-4xl text-white justify-center text-center">
+                                0
+                                </Typography>
+                            </CardBody>
+                            <div className='bg-gray-600 w-full py-4'>
+                                <Typography className='justify-center text-center text-lg text-white'>
                                 Others
                                 </Typography>
-                            </CardHeader>
-                            <CardBody className="flex flex-col gap-4">
-                                <Typography variant='h1' className='text-center'>0</Typography>
-                            </CardBody>
-                            <CardFooter className="pt-0">
-                                <Typography variant='small'>Others</Typography>
-                            </CardFooter>
+                            </div>
                         </Card>
+                    </div>
 
+                    <div className='grid grid-cols-5 gap-3'>
+                        {/* Content goes here */}
+                        <Card className="mt-6 ml-6 h-40 w-56 flex-col justify-center items-center bg-white">
+                            <CardBody>
+                                <Typography variant="h1" color="blue-gray" className="mb-2 ml-6 7xl font-bold text-4xl text-red-500 flex">
+                                1,733
+                                <ListItemSuffix>
+                                   <IoPersonAdd size={40} />
+                                </ListItemSuffix>
+                                </Typography>
+                                <Typography className='justify-center text-center text-sm text-black'>
+                                Julia Burke Maternity Centre
+                                </Typography>
+                            </CardBody>
+                            </Card>
+
+                            <Card className="mt-6 h-40 w-56 flex-col justify-center items-center bg-white">
+                            <CardBody>
+                                <Typography variant="h1" color="blue-gray" className="mb-2 ml-6 font-bold text-4xl text-purple-500 flex">
+                                1,653
+                                <ListItemSuffix>
+                                   <IoPersonAdd size={40} />
+                                </ListItemSuffix>
+                                </Typography>
+                                <Typography className='justify-center text-center text-sm text-black'>
+                                Godwin Onyema Maternity Centre
+                                </Typography>
+                            </CardBody>
+                            </Card>
+
+                            <Card className="mt-6 h-40 w-56 flex-col justify-center items-center bg-white">
+                            <CardBody>
+                                <Typography variant="h1" color="blue-gray" className="mb-2 ml-6 font-bold text-4xl text-red-700 flex">
+                                 1,949
+                                 <ListItemSuffix>
+                                   <IoPersonAdd size={40} />
+                                </ListItemSuffix>
+                                </Typography>
+                                <Typography className='justify-center text-center text-sm text-black'>
+                                John & Terry Levin Family Centre
+                                </Typography>
+                            </CardBody>
+                            </Card>
+
+                            <Card className="mt-6 h-40 w-56 flex-col justify-center items-center bg-white">
+                            <CardBody>
+                                <Typography variant="h1" color="blue-gray" className="mb-2 ml-6 font-bold text-4xl text-green-500 flex">
+                                471
+                                <ListItemSuffix>
+                                   <IoPersonAdd size={40} />
+                                </ListItemSuffix>
+                                </Typography>
+                                <Typography className='justify-center text-center text-sm text-black'>
+                                Zegar Jackson
+                                </Typography>
+                            </CardBody>
+                            </Card>
+
+                            <Card className="mt-6 h-40 w-56 flex-col justify-center items-center bg-white">
+                            <CardBody>
+                                <Typography variant="h1" color="blue-gray" className="mb-2 ml-6 font-bold text-4xl text-blue-900 flex">
+                                1,578
+                                <ListItemSuffix>
+                                   <IoPersonAdd size={40} />
+                                </ListItemSuffix>
+                                </Typography>
+                                <Typography className='justify-center text-center text-sm text-black'>
+                                 Robin & Estelle & Clarke Family Centre
+                                </Typography>
+                            </CardBody>
+                        </Card>
+                    </div>
+
+                    <div className="grid h-96 w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
+                        <img
+                                class="object-cover object-center w-full rounded-lg h-96"
+                                src="https://trinityweb.net/wp-content/uploads/2023/04/geanco_app_landing.jpg"
+                                alt="GEANCO"
+                            />      
                     </div>
                 </div>
             </main>
