3861d26533d88bbee425c9846b3f946c37f4526b
Dimmaweb20 <chidimaumeayo@gmail.com>
2024-04-11 17:38:19 +0100

    Worked on surgery Api

--- app/api/surgery/route.js
+++ app/api/surgery/route.js
@@ -6,8 +6,8 @@ export async function POST(req) {
         const data = await req.json()
         
 
-        const antenatal = await prisma.antenatal.create({ data: data })
-        return NextResponse.json({ data: antenatal, message: "Antenatal created!" }, { status: 201 });
+        const surgery = await prisma.surgery.create({ data: data })
+        return NextResponse.json({ data: surgery, message: "Surgery Record created!" }, { status: 201 });
     } catch (error) {
         return NextResponse.json({ data: error });
     }
@@ -20,8 +20,8 @@ export async function GET(req, context) {
         // const { params } = context
         // console.log("PARAMS =:", params);
 
-        const antenatal = await prisma.antenatal.findMany();
-        return NextResponse.json({ data: antenatal }, { status: 200 })
+        const surgery = await prisma.surgery.findMany();
+        return NextResponse.json({ data: surgery }, { status: 200 })
     } catch (error) {
         return NextResponse.json({ data: error.message }, { status: 500 })
     }
@@ -32,8 +32,8 @@ export async function PUT(req) {
     try {
         const data = await req.json()
 
-        const antenatal = await prisma.antenatal.update({ where: { id: data.id }, data: data })
-        return NextResponse.json({ data: antenatal, message: "Antenatal record updated" }, { status: 200 })
+        const surgery = await prisma.surgery.update({ where: { id: data.id }, data: data })
+        return NextResponse.json({ data: surgery, message: "Surgery record updated" }, { status: 200 })
     } catch (error) {
         return NextResponse.json({ data: error.message }, { status: 500 })
     }
@@ -44,10 +44,10 @@ export async function DELETE(req) {
     try {
         const data = await req.json()
 
-        const findpatient = await prisma.antenatal.findFirst({ where: { id: data.id } })
+        const findpatient = await prisma.surgery.findFirst({ where: { id: data.id } })
         if (findpatient) {
-            const antenatal = await prisma.antenatal.delete({ where: { id: data.id } })
-            return NextResponse.json({ data: antenatal, message: "Antenatal Record Deleted" }, { status: 200 })
+            const surgery = await prisma.surgery.delete({ where: { id: data.id } })
+            return NextResponse.json({ data: surgery, message: "Surgery Record Deleted" }, { status: 200 })
         } else {
             return NextResponse.json({ data: "Patient with id not found!" }, { status: 404 })
         }
