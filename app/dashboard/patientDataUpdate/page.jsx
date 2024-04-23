b94385400c24f52e1707e17f47770b6fe35efe30
Godson Pius <40927597+godson-pius@users.noreply.github.com>
2024-04-23 17:38:56 +0100

    Fixed update page

--- app/dashboard/patientDataUpdate/page.jsx
+++ app/dashboard/patientDataUpdate/page.jsx
@@ -17,13 +17,16 @@ import {
     Option
 } from "@material-tailwind/react";
 import { IoCreate, IoCreateOutline, IoPrintOutline, IoTrashOutline } from 'react-icons/io5';
-import { getPatientData } from '@/controllers'
+import { getPatientData, deletePatient } from '@/controllers'
+import moment from 'moment/moment';
+import { toast } from 'react-toastify'
 
 
 const page = () => {
     const [open, setOpen] = useState(false);
     const [patients, setPatients] = useState([])
     const [singlePatient, setSinglePatient] = useState()
+    const [search, setSearch] = useState("")
     const TABLE_HEAD = ["Full Name", "Health Institution", "Mobile", "Clinic ID", "Patient Category"];
 
     const handleGetPatients = async () => {
@@ -32,7 +35,32 @@ const page = () => {
     }
 
     const handleGetSinglePatient = (id) => {
-        console.log(patients.find((e) => e._id == id));
+        const patient = patients.find((e) => e.id == id)
+        setSinglePatient(patient)
+
+        // Open patient modal
+        setOpen(true)
+    }
+
+    const handleSearch = (e) => {
+        const searched = patients.filter()
+        console.log(searched)
+    }
+
+    const handleDeletePatient = async () => {
+        const data = { id: singlePatient?.id }
+        const conf = confirm("Are you sure you want to delete?")
+
+        if (conf) {
+            const res = await deletePatient(data)
+
+            if (res.ok) {
+                toast.success("Patient deleted successfully")
+                handleGetPatients()
+            } else {
+                toast.error(res.data)
+            }
+        }
     }
 
     useEffect(() => {
@@ -53,7 +81,7 @@ const page = () => {
                                 <Typography variant='h5'>Patient Data & Update</Typography>
 
                                 <div className='w-96'>
-                                    <Input variant='outlined' size='sm' color='blue' placeholder='search' label='search by: [name, mobile, institution, clinic id, category]' />
+                                    <Input variant='outlined' size='sm' color='blue' placeholder='search' label='search by: [name, mobile, institution, clinic id, category]' onChange={(e) => setSearch(e.target.value)} />
                                 </div>
                             </CardHeader>
                             <CardBody className='mt-1'>
@@ -77,61 +105,72 @@ const page = () => {
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        {patients.map((data, index) => {
-                                            const isLast = index === patients.length - 1;
-                                            const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50 cursor-pointer";
+                                        {patients.filter((user) => (search.toLowerCase().trim() == "" ? patients : user.lastname.toLowerCase().includes(search) ||
+                                            user.firstname.toLowerCase().includes(search) ||
+                                            user.lastname.toLowerCase().includes(search) ||
+                                            user.email.toLowerCase().includes(search) ||
+                                            user.healthinstitution.toLowerCase().includes(search) ||
+                                            user.clinicid.toLowerCase().includes(search) ||
+                                            user.gender.toLowerCase().includes(search)) ||
+                                            user.mobile.toLowerCase().includes(search) ||
+                                            user.id.toLowerCase().includes(search) ||
+                                            user.patientcategory.toLowerCase().includes(search) ||
+                                            user.dateofbirth.toLowerCase().includes(search) ||
+                                            user.residentialaddress.toLowerCase().includes(search)).map((data, index) => {
+                                                const isLast = index === patients.length - 1;
+                                                const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50 cursor-pointer";
 
-                                            return (
-                                                <tr key={index}>
-                                                    <td className={classes} onClick={() => handleGetSinglePatient(data?._id)}>
-                                                        <Typography
-                                                            variant="small"
-                                                            color="blue-gray"
-                                                            className="font-normal cursor-pointer"
+                                                return (
+                                                    <tr key={index}>
+                                                        <td className={classes} onClick={() => handleGetSinglePatient(data?.id)}>
+                                                            <Typography
+                                                                variant="small"
+                                                                color="blue-gray"
+                                                                className="font-normal cursor-pointer"
 
-                                                        >
-                                                            {data?.firstname} {data?.lastname}
-                                                        </Typography>
-                                                    </td>
-                                                    <td className={classes}>
-                                                        <Typography
-                                                            variant="small"
-                                                            color="blue-gray"
-                                                            className="font-normal"
-                                                        >
-                                                            {data?.healthinstitution}
-                                                        </Typography>
-                                                    </td>
-                                                    <td className={classes}>
-                                                        <Typography
-                                                            variant="small"
-                                                            color="blue-gray"
-                                                            className="font-normal"
-                                                        >
-                                                            {data?.mobile}
-                                                        </Typography>
-                                                    </td>
-                                                    <td className={classes}>
-                                                        <Typography
-                                                            variant="small"
-                                                            color="blue-gray"
-                                                            className="font-normal"
-                                                        >
-                                                            {data?.clinicid}
-                                                        </Typography>
-                                                    </td>
-                                                    <td className={classes}>
-                                                        <Typography
-                                                            variant="small"
-                                                            color="blue-gray"
-                                                            className="font-normal"
-                                                        >
-                                                            {data?.patientcategory}
-                                                        </Typography>
-                                                    </td>
-                                                </tr>
-                                            );
-                                        })}
+                                                            >
+                                                                {data?.firstname} {data?.lastname}
+                                                            </Typography>
+                                                        </td>
+                                                        <td className={classes}>
+                                                            <Typography
+                                                                variant="small"
+                                                                color="blue-gray"
+                                                                className="font-normal"
+                                                            >
+                                                                {data?.healthinstitution}
+                                                            </Typography>
+                                                        </td>
+                                                        <td className={classes}>
+                                                            <Typography
+                                                                variant="small"
+                                                                color="blue-gray"
+                                                                className="font-normal"
+                                                            >
+                                                                {data?.mobile}
+                                                            </Typography>
+                                                        </td>
+                                                        <td className={classes}>
+                                                            <Typography
+                                                                variant="small"
+                                                                color="blue-gray"
+                                                                className="font-normal"
+                                                            >
+                                                                {data?.clinicid}
+                                                            </Typography>
+                                                        </td>
+                                                        <td className={classes}>
+                                                            <Typography
+                                                                variant="small"
+                                                                color="blue-gray"
+                                                                className="font-normal"
+                                                            >
+                                                                {data?.patientcategory}
+                                                            </Typography>
+                                                        </td>
+                                                    </tr>
+                                                );
+                                            })}
                                     </tbody>
                                 </table>
                             </CardBody>
@@ -161,9 +200,9 @@ const page = () => {
                                         />
                                     </svg>
                                 </IconButton>
-                                <IoPrintOutline size={20} />
-                                <IoCreateOutline size={20} />
-                                <IoTrashOutline size={20} />
+                                <IoPrintOutline size={20} onClick={() => window.print()} className='cursor-pointer' />
+                                <IoCreateOutline size={20} className='cursor-pointer' />
+                                <IoTrashOutline size={20} onClick={handleDeletePatient} className='cursor-pointer' />
                             </div>
                         </div>
 
@@ -171,42 +210,42 @@ const page = () => {
 
                             <div className="flex border-2 justify-between items-center">
                                 <p className='bg-gray-100 w-96 p-2'>Patient Id</p>
-                                <p className='pr-2'>7504</p>
+                                <p className='pr-2'>{singlePatient?.id}</p>
                             </div>
 
                             <div className="flex border-2 justify-between items-center">
                                 <p className='bg-gray-100 w-96 p-2'>Registration Date</p>
-                                <p className='pr-2'>04-Mar-2024</p>
+                                <p className='pr-2'>{moment(singlePatient?.recordentrydate).format("MMMM Do, YYYY")}</p>
                             </div>
 
                             <div className="flex border-2 justify-between items-center">
                                 <p className='bg-gray-100 w-96 p-2'>Full Name</p>
-                                <p className='pr-2'>Igwe Peace</p>
+                                <p className='pr-2'>{`${singlePatient?.firstname} ${singlePatient?.lastname}`}</p>
                             </div>
 
                             <div className="flex border-2 justify-between items-center">
                                 <p className='bg-gray-100 w-96 p-2'>Gender</p>
-                                <p className='pr-2'>Female</p>
+                                <p className='pr-2'>{singlePatient?.gender}</p>
                             </div>
 
                             <div className="flex border-2 justify-between items-center">
                                 <p className='bg-gray-100 w-96 p-2'>Date of birth</p>
-                                <p className='pr-2'>07-Aug-2003</p>
+                                <p className='pr-2'>{moment(singlePatient?.dateofbirth).format("MMMM Do, YYYY")}</p>
                             </div>
 
                             <div className="flex border-2 justify-between items-center">
                                 <p className='bg-gray-100 w-96 p-2'>Mobile</p>
-                                <p className='pr-2'>+2349032993933</p>
+                                <p className='pr-2'>{singlePatient?.mobile}</p>
                             </div>
 
                             <div className="flex border-2 justify-between items-center">
                                 <p className='bg-gray-100 w-96 p-2'>Email</p>
-                                <p className='pr-2'>+2349032993933</p>
+                                <p className='pr-2'>{singlePatient?.email}</p>
                             </div>
 
                             <div className="flex border-2 justify-between items-center">
                                 <p className='bg-gray-100 w-96 p-2'>Residential Address</p>
-                                <p className='pr-2'>Okpuno, Awka-North, Awka</p>
+                                <p className='pr-2'>{singlePatient?.residentialaddress}</p>
                             </div>
 
                         </section>
